238184d507a4f0e1a1225be888731ae8af21a936
Guilhem SEYVET <guilhemcvdev@outlook.com>
2022-02-02 15:00:45 +0100

    route vin complete avec INNER JOIN

--- models/vin.js
+++ models/vin.js
@@ -2,7 +2,11 @@ const connection = require('../db-config');
 const db = connection.promise();
 
 const getWine = () => {
-  return db.query('SELECT * FROM vin').then((result) => result);
+  return db
+    .query(
+      'SELECT * FROM vin INNER JOIN type_vin ON vin.id_type_vin = type_vin.id INNER JOIN region ON vin.id_region = region.id'
+    )
+    .then((result) => result);
 };
 
 module.exports = { getWine };
